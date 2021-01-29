24cc807e2104ba91a6eb757450f11e24b1a8f608
Pyncialx <78092883+Pyncialx@users.noreply.github.com>
2021-01-29 22:44:39 +0100

    ManagerCheck per platform

--- Assets/Scripts/MattiaScript/PlatformScript.cs
+++ Assets/Scripts/MattiaScript/PlatformScript.cs
@@ -20,7 +20,7 @@ public class PlatformScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //orientare verso il centro!
+        
        
         if (goToDestination)
         {
