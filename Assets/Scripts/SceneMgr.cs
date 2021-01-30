7337412b61d5007deea16594f7e465b666c785f9
Davjen <55961996+Davjen@users.noreply.github.com>
2021-01-30 16:08:09 +0100

    Adjustament mgr, changescene,

---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneMgr : MonoBehaviour
+{
+    public Image image;
+    bool fade;
+    float alpha = 0;
+    AudioSource audioSource;
+    public float test;
+    public float currentTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    void PlayMusic()
+    {
+        audioSource.Play();
+    }
+
+    void StopMusic()
+    {
+        audioSource.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = audioSource.time;
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            test = audioSource.time;
+            StopMusic();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            audioSource.time = test;
+
+            PlayMusic();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            fade = true;
+        }
+        if (fade)
+        {
+
+            alpha += 0.9f * Time.deltaTime;
+            image.color = new Color(0, 0, 0, alpha);
+            if (image.color.a >= 1)
+            {
+
+                SceneManager.LoadScene("MattiaScene");
+            }
+        }
+    }
+}
