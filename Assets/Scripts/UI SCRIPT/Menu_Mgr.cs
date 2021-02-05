fbc3c550028d656fabcd5c31d4c9739167c3fb3a
AssoAndrea <andrea1944@live.it>
2021-02-05 20:15:56 +0100

    MenuCameraFix

--- Assets/Scripts/UI SCRIPT/Menu_Mgr.cs
+++ Assets/Scripts/UI SCRIPT/Menu_Mgr.cs
@@ -110,6 +110,10 @@ public class Menu_Mgr : MonoBehaviour
         {
             GoToMainMenu();
         }
+        if (fadeToStartGame)
+        {
+            FadeToStartGame();
+        }
 
         if (startToFade)
         {
